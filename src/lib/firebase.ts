33f843addf6c02204c23bc1add68f8c0352d84b0
phiexpesc <95003624+phiexpesc@users.noreply.github.com>
2026-03-17 03:16:44 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN-EdkB55wZERcSG_a3VIWu4Kg8caQu7A",
-  authDomain: "azmovie-cloud.firebaseapp.com",
-  projectId: "azmovie-cloud",
-  storageBucket: "azmovie-cloud.firebasestorage.app",
-  messagingSenderId: "505489860753",
-  appId: "1:505489860753:web:dc6579c539ed5646633730"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
